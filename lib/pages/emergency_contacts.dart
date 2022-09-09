c9baed7c621df958dbf52b05c5ad1ff582796a60
Laksh Bharani <80910691+LakshBharani@users.noreply.github.com>
2022-09-10 03:14:56 +0530

    Create emergency_contacts.dart

---
+++ lib/pages/emergency_contacts.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class EmergencyContactsPage extends StatefulWidget {
+  const EmergencyContactsPage({super.key});
+
+  @override
+  State<EmergencyContactsPage> createState() => _EmergencyContactsPageState();
+}
+
+class _EmergencyContactsPageState extends State<EmergencyContactsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
